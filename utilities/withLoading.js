138032165436f244ca83a31b617e4585e403a704
Gonzalo <bilunegonzalo@gmail.com>
2018-12-29 15:05:11 -0300

    Modifico comentario del componente

--- utilities/withLoading.js
+++ utilities/withLoading.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 import routerEvents from "next-router-events";
 
+/**
+ * HOC que permite a los componentes saber cuando se está haciendo un fetch en la
+ * aplicación. Da la posibilidad de que en ese lapso de tiempo se muestre un Loader
+ * que se pasa por parámetro o se le pase la propiedad loading al componente en cuestión.
+ * @param {React Element} Loader Componente que se renderiza mientras se hace una request.
+ */
 const withLoading = (Loader) => (MyComponent) => class Loading extends Component {
 	constructor(props) {
 		super(props);
@@ -20,6 +26,8 @@ const withLoading = (Loader) => (MyComponent) => class Loading extends Component
 
 	componentWillUnmount() {
 		if (this.timeOut) {
+			// Si se desmonta antes de que finalice el período
+			// .5s en el que no se muestra loader, cancela el timeout.
 			clearTimeout(this.timeOut);
 		}
 
@@ -27,6 +35,11 @@ const withLoading = (Loader) => (MyComponent) => class Loading extends Component
 		routerEvents.off("routeChangeComplete", this.pageLoadEnd);
 	}
 
+	/**
+	 * Función que se ejecuta cuando comienza el cambio
+	 * de ruta. Si la transición de pages tarda más de .5s
+	 * setea el estado del componente en loading: true.
+	 */
 	pageLoadStart = url => {
 		const { pathname, search } = window.location;
 		const currentUrl = pathname + search;
@@ -35,10 +48,14 @@ const withLoading = (Loader) => (MyComponent) => class Loading extends Component
 				if (!this.state.loading) {
 					this.setState({ loading: true });
 				}
-			}, 500)
+			}, 500);
 		}
 	}
 
+	/**
+	 * Función que se ejecuta cuando finaliza
+	 * el cambio de ruta. Setea el estado del componente en loading:false
+	 */
 	pageLoadEnd = url => {
 		if (this.state.loading) {
 			this.setState({ loading: false });
