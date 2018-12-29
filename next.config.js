9ea4cadc927baca975275cf1d380ef58aa6db9fa
Gonzalo <bilunegonzalo@gmail.com>
2018-12-29 11:52:53 -0300

    scss minifier/autoprefixer

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
 const withSass = require('@zeit/next-sass');
 
-module.exports = withSass();
+module.exports = withSass({
+	sassLoaderOptions: {
+		includePaths: ['theme'],
+		outputStyle: 'compressed',
+	},
+});
